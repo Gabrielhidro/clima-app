50de863294cf55b1ef054aad25428b477bf679b8
gabriel.freitas <gabriel.freitas@woli.com.br>
2025-03-19 19:13:50 -0300

    Create lint configs, dependencies and create fetchData function

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,16 +1,28 @@
-import { dirname } from "path";
-import { fileURLToPath } from "url";
-import { FlatCompat } from "@eslint/eslintrc";
+import js from '@eslint/js'
+import { dirname } from 'path'
+import { fileURLToPath } from 'url'
+import { FlatCompat } from '@eslint/eslintrc'
+import prettierPlugin from 'eslint-plugin-prettier'
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = dirname(__filename)
 
 const compat = new FlatCompat({
   baseDirectory: __dirname,
-});
+  recommendedConfig: js.configs.recommended
+})
 
 const eslintConfig = [
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
-];
+  ...compat.extends('next/core-web-vitals', 'next/typescript', 'eslint:recommended', 'next'),
+  {
+    plugins: {
+      prettier: prettierPlugin
+    },
+    rules: {
+      'prettier/prettier': 'error',
+      '@typescript-eslint/no-explicit-any': 'off'
+    }
+  }
+]
 
-export default eslintConfig;
+export default eslintConfig
---
+++ src/app/utils/fetchData.ts
@@ -0,0 +1,31 @@
+import axios from 'axios'
+
+interface WeatherData {
+  name: any
+  main: any
+  weather: any
+  wind: any
+}
+
+export async function fetchWeather(city: string): Promise<WeatherData> {
+  const apiKey = process.env.NEXT_PUBLIC_OPENWEATHERMAP_API_KEY
+
+  if (!apiKey) {
+    throw new Error('Chave incorreta')
+  }
+
+  try {
+    const response = await axios.get(
+      `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric&lang=pt_br`
+    )
+
+    if (response.status !== 200 || !response.data) {
+      throw new Error('Erro ao buscar dados do clima.')
+    }
+
+    return response.data
+  } catch (error) {
+    console.error('Erro na requisição à API:', error)
+    throw new Error('Não foi possível obter os dados do clima. Verifique o nome da cidade.')
+  }
+}
